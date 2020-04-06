a9236ae1aa35e89d7d68d4fa7b23964301afcd7a
myk245 <myk245@nyu.edu>
2020-04-06 14:12:37 -0400

    added handler for More button

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -3,9 +3,14 @@ import MoreButton from '../components/MoreButton';
 import Sushi from '../components/Sushi'; 
 
 class SushiContainer extends Component {
+  state = {
+    sushiToShow: 4,
+    showMore: false
+  }
+
   renderSushi = () => {
     return (
-      this.props.sushis.slice(0, 4).map(sushi =>
+      this.props.sushis.slice(0, this.state.sushiToShow).map(sushi =>
         <Sushi
           key={sushi.id}
           name={sushi.name}
@@ -17,13 +22,21 @@ class SushiContainer extends Component {
     )
   }
 
+  // when "More" button is clicked, change showMore to true
+  // render the next 4 sushi
+  handleMoreClick = () => {
+    this.setState({
+      showMore: true
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="belt">
         {/* Render Sushi components here! */ }
           {this.renderSushi()}
-          <MoreButton />
+          <MoreButton handleMoreClick={this.handleMoreClick} />
         </div>
       </div>
     )
