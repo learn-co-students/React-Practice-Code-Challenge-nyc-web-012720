7bb1498231e197fd73d813a9db379a683748099b
mdiaz26 <mike.diaz006@gmail.com>
2020-04-06 13:39:30 -0400

    rendered sushi

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,14 +3,25 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis/"
 
 class App extends Component {
+
+  state = {
+    sushis: [],
+    eatenSushis: []
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(response => response.json())
+    .then(sushis => this.setState({sushis: sushis.slice(0,4)}))
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={this.state.sushis}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -5,16 +5,16 @@ const Sushi = (props) => {
     <div className="sushi">
       <div className="plate" 
            onClick={/* Give me a callback! */ null}>
-        { 
+        {
           /* Tell me if this sushi has been eaten! */ 
           false ?
             null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={props.img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.name} - ${props.price}
       </h4>
     </div>
   )
